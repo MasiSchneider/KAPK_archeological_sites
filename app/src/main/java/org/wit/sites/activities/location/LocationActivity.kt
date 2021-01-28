b7831edda7cdff0ab4874185fb16893fac4cd3db
scm41809 <marcel.schneider@st.oth-regensburg.de>
2021-01-28 23:28:06 +0100

    V3 - Additional feature: Current Location Detection

--- app/src/main/java/org/wit/sites/activities/location/LocationActivity.kt
+++ app/src/main/java/org/wit/sites/activities/location/LocationActivity.kt
@@ -1,71 +1,114 @@
 package org.wit.sites.activities.location
 
-import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
+import kotlinx.android.synthetic.main.activity_map.*
+
 import org.wit.sites.R
 import org.wit.sites.models.Location
 
-class LocationActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarkerDragListener, GoogleMap.OnMarkerClickListener{
+class LocationActivity : AppCompatActivity(), GoogleMap.OnMarkerDragListener, GoogleMap.OnMarkerClickListener{
 
-    private lateinit var map: GoogleMap
     var location = Location()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map)
         location = intent.extras?.getParcelable<Location>("location")!!
-        val mapFragment = supportFragmentManager
-                .findFragmentById(R.id.map) as SupportMapFragment
-        mapFragment.getMapAsync(this)
-    }
 
-    override fun onMapReady(googleMap: GoogleMap) {
-        map = googleMap
-        val loc = LatLng(location.lat, location.lng)
-        val options = MarkerOptions()
-                .title("Site")
+
+        toolbarMap.title = title
+        setSupportActionBar(toolbarMap)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync {
+            it.setOnMarkerDragListener(this)
+            it.setOnMarkerClickListener(this)
+            val loc = LatLng(location.lat, location.lng)
+            val options = MarkerOptions()
+                .title("Placemark")
                 .snippet("GPS : " + loc.toString())
                 .draggable(true)
                 .position(loc)
-        map.addMarker(options)
-        map.setOnMarkerDragListener(this)
-        map.setOnMarkerClickListener(this)
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(loc, location.zoom))
+            it.addMarker(options)
+            it.moveCamera(CameraUpdateFactory.newLatLngZoom(loc, location.zoom))
+            lat.setText("%.6f".format(location.lat))
+            lng.setText("%.6f".format(location.lng))
+        }
     }
 
-    override fun onMarkerClick(marker: Marker): Boolean {
-        val loc = LatLng(location.lat, location.lng)
-        marker.setSnippet("GPS : " + loc.toString())
-        return false
+
+
+
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_edit_location, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item?.itemId) {
+            R.id.item_save -> {
+                val resultIntent = Intent()
+                resultIntent.putExtra("location", location)
+                setResult(0, resultIntent)
+                finish()
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     override fun onMarkerDragStart(marker: Marker) {
     }
 
     override fun onMarkerDrag(marker: Marker) {
+        lat.setText("%.6f".format(marker.position.latitude))
+        lng.setText("%.6f".format(marker.position.longitude))
     }
 
     override fun onMarkerDragEnd(marker: Marker) {
         location.lat = marker.position.latitude
         location.lng = marker.position.longitude
-        location.zoom = map.cameraPosition.zoom
     }
 
-    override fun onBackPressed() {
-        val resultIntent = Intent()
-        resultIntent.putExtra("location", location)
-        setResult(Activity.RESULT_OK, resultIntent)
-        finish()
-        super.onBackPressed()
+    override fun onMarkerClick(marker: Marker): Boolean {
+        val loc = LatLng(location.lat, location.lng)
+        marker.snippet = "GPS : $loc"
+        return false
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView.onSaveInstanceState(outState)
     }
 }
